bc9c168845c60b5d7e7a559f8dcc5868b7738b5a
Evandro Monteiro Lima <goodvandro@gmail.com>
2023-12-08 15:09:28 +0000

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "goodvandro/go-opportunities.git/router"
+import (
+	"goodvandro/go-opportunities.git/config"
+	"goodvandro/go-opportunities.git/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errof("config init error: %v", err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }

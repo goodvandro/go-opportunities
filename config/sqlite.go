b8cce78d1cb4fb6e9eb940cad7818fcb417912bb
Evandro Monteiro Lima <goodvandro@gmail.com>
2023-12-11 10:17:18 +0000

    feat: add database handler

---
+++ config/sqlite.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"goodvandro/go-opportunities.git/schemas"
+	"os"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitializeSQLite() (*gorm.DB, error) {
+	logger := GetLogger("sqlite")
+	dbPath := "./db/main.db"
+	// Check if the database file exists
+	_, err := os.Stat(dbPath)
+	if os.IsNotExist(err) {
+		logger.Info("database file not found, creating...")
+		// Create the database file and directory
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+		file, err := os.Create(dbPath)
+		if err != nil {
+			return nil, err
+		}
+		file.Close()
+	}
+	//Create DB and connect
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		logger.Errof("aqlite opening error: %v", err)
+		return nil, err
+	}
+	// migrate the Schema
+	err = db.AutoMigrate(&schemas.Openings{})
+	if err != nil {
+		logger.Errof("aqlite automigration error: %v", err)
+		return nil, err
+	}
+	// Return the DB
+	return db, nil
+}
--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-sqlite3 v1.14.18 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
@@ -31,5 +32,6 @@ require (
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/sqlite v1.5.4 // indirect
 	gorm.io/gorm v1.25.5 // indirect
 )
